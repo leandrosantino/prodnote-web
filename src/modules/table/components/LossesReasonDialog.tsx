b3d898dc972201bdd52d5e91a0cb10f067877b42
adler-pelzer-dev <adleroee@gmail.com>
2025-02-07 09:35:37 -0300

    update

--- src/modules/table/components/LossesReasonDialog.tsx
+++ src/modules/table/components/LossesReasonDialog.tsx
@@ -16,28 +16,34 @@ export function LossesReasonDialog({ children, efficiencyRecord }: {children: Re
       <DialogTrigger asChild>
         {children}
       </DialogTrigger>
-      <DialogContent className="md:min-w-[700px] md:min-h-[500px] max-sm:h-screen max-sm:w-screen flex flex-col">
+      <DialogContent className="md:min-w-[700px] md:min-h-[400px] max-sm:h-screen max-sm:w-screen flex flex-col">
         <DialogHeader>
           <DialogTitle>Perdas de Eficiência</DialogTitle>
           <DialogDescription>
             {efficiencyRecord.date.toLocaleDateString()} - {efficiencyRecord.hourInterval} - {efficiencyRecord.productionProcessId}
           </DialogDescription>
         </DialogHeader>
-        <div className="grid gap-4 py-4 overflow-auto max-h-[400px]">
-          {efficiencyRecord.productionEfficiencyLosses.map((item, index) => (
-            <Card key={index} >
-              <CardHeader>
-                <CardTitle>{item.classification}</CardTitle>
-              </CardHeader>
-              <CardContent>
-                <p>{item.description}</p>
-                <p>
-                  {item.classification === 'Scrap + Quality Issues' ? 'Peças: ' : 'Tempo (min): '}
-                  {item.lostTimeInMinutes}
-                </p>
-              </CardContent>
-            </Card>
-          ))}
+        <div className="grid gap-4 py-4 overflow-auto max-h-[300px]">
+          {
+            efficiencyRecord.productionEfficiencyLosses.length === 0 ?
+            <>
+              <span className="text-muted-foreground w-full h-[200px] flex justify-center items-center" >Nenhuma perda apontada...</span>
+            </> :
+            efficiencyRecord.productionEfficiencyLosses.map((item, index) => (
+              <Card key={index} >
+                <CardHeader>
+                  <CardTitle>{item.classification}</CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <p>{item.description}</p>
+                  <p>
+                    {item.classification === 'Scrap + Quality Issues' ? 'Peças: ' : 'Tempo (min): '}
+                    {item.lostTimeInMinutes}
+                  </p>
+                </CardContent>
+              </Card>
+            ))
+          }
         </div>
       </DialogContent>
     </Dialog>
